c03a6af3da9f11f9c44d818d4f2e04e3f3c8b376
Amritpalkaur18 <palamrit0218@gmail.com>
2024-10-09 15:21:53 -0400

    Add project files.

---
+++ MathOperations 2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace MathOperationApp
+{
+   
+    public class MathOperations
+    {
+       
+        public void PerformOperation(int firstNumber, int secondNumber)
+        {
+           
+            int result = firstNumber * 2;
+
+           
+            Console.WriteLine($"The result of the math operation on {firstNumber} is {result}");
+            Console.WriteLine($"The second integer is {secondNumber}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MathOperations mathOps = new MathOperations();
+
+           
+            mathOps.PerformOperation(5, 10);
+
+           
+            mathOps.PerformOperation(firstNumber: 8, secondNumber: 15);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
